7f183e884546cff46236074d270d477c029a70cb
lookyee <57711440+lookyee@users.noreply.github.com>
2020-08-22 02:01:08 +0700

    update

--- service/product.php
+++ service/product.php
@@ -3,16 +3,18 @@
     header('Content-type: text/html; charset=utf-8');
     require_once 'PDO.php';
 
-    $db_host = 'localhost:8889';
+    $db_host = 'localhost';
     $db_name = 'matchminton';
     $db_user = 'root';
-    $db_pass = '1';
+    $db_pass = '';
 
     $conn = new PDO("mysql:host=$db_host; dbname=$db_name", $db_user, $db_pass);
     $conn->exec("SET CHARACTER SET utf8");
 
     if(isset($_POST['action']) && $_POST['action'] == 'create'){
-        $sql = "INSERT INTO `product`(`product_name`, `price`, `description`, `quantity`, `brand_id`, `type`)VALUES ('".$_POST['product_name']."','".$_POST['price']."','".$_POST['description']."','".$_POST['quantity']."','".$_POST['brand_id']."','".$_POST['type']."')";
+        $sql = "INSERT INTO `product`(`product_name`, `price`, `description`, `quantity`, `brand_id`, `type`) VALUES ('".$_POST['product_name']."','".$_POST['price']."','".$_POST['description']."','".$_POST['quantity']."','".$_POST['brand_id']."','".$_POST['type']."')";
+        
+        echo $sql;
         $rs = getpdo($conn,$sql);
         if($rs){
         	$lastid = $conn->lastInsertId();
