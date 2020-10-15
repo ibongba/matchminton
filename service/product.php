f79770f027dfdd155444067be8e37f21071d7b15
Anuparp Wijanpreecha <anuparp.w@sahapat.co.th>
2020-10-15 18:21:46 +0700

    update

--- service/product.php
+++ service/product.php
@@ -3,10 +3,10 @@
     header('Content-type: text/html; charset=utf-8');
     require_once 'PDO.php';
 
-    $db_host = 'localhost:8889';
+    $db_host = 'localhost';
     $db_name = 'matchminton';
     $db_user = 'root';
-    $db_pass = '1';
+    $db_pass = '';
 
     $conn = new PDO("mysql:host=$db_host; dbname=$db_name", $db_user, $db_pass);
     $conn->exec("SET CHARACTER SET utf8");
