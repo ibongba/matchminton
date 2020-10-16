378e4e66784cee49a45675e4583c862e5e288f74
Anuparp Wijanpreecha <I.bong.ba@gmail.com>
2020-10-16 19:13:08 +0700

    update

---
+++ service/themes.php
@@ -0,0 +1,15 @@
+<?php
+    require_once 'config.php';
+
+
+    if(isset($_POST['action']) && $_POST['action'] == 'create_theme'){
+    	
+        $sql = "INSERT INTO `theme`(`content`, `title`, `category`, `fk_user_id`) VALUES ('$_POST['summernote']','$_POST['header']','$_POST['category']','$_POST['user_id']')";
+        $rs = getpdo($conn,$sql);
+        if(isset($rs) && count($rs) > 0){
+        	$res = array("code" => 200, "result" => $rs[0]);
+        	echo json_encode($res);
+            return ;
+        }
+    }
+?>
