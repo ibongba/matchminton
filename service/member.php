7435448ed8ecd5ec43f9335d657da317194f1a03
Anuparp Wijanpreecha <I.bong.ba@gmail.com>
2020-10-16 19:32:19 +0700

    update

--- service/member.php
+++ service/member.php
@@ -1,15 +1,5 @@
 <?php
-	date_default_timezone_set("Asia/Bangkok");
-    header('Content-type: text/html; charset=utf-8');
-    require_once 'PDO.php';
-
-    $db_host = 'localhost';
-    $db_name = 'matchminton';
-    $db_user = 'root';
-    $db_pass = '';
-
-    $conn = new PDO("mysql:host=$db_host; dbname=$db_name", $db_user, $db_pass);
-    $conn->exec("SET CHARACTER SET utf8");
+    require_once 'config.php';
 
     if(isset($_POST['action']) && $_POST['action'] == 'login'){
         $sql = "SELECT `user_id`, `name`, `surname` ,`email` ,`phone` FROM `users` WHERE `username` = '".$_POST['username']."' and `password` = '".md5($_POST['password'])."' and status=1";
--- service/product.php
+++ service/product.php
@@ -1,16 +1,6 @@
 <?php
-	date_default_timezone_set("Asia/Bangkok");
-    header('Content-type: text/html; charset=utf-8');
-    require_once 'PDO.php';
-
-    $db_host = 'localhost';
-    $db_name = 'matchminton';
-    $db_user = 'root';
-    $db_pass = '';
-
-    $conn = new PDO("mysql:host=$db_host; dbname=$db_name", $db_user, $db_pass);
-    $conn->exec("SET CHARACTER SET utf8");
-
+    require_once 'config.php';
+    
     if(isset($_POST['action']) && $_POST['action'] == 'create'){
         $sql = "INSERT INTO `product`(`product_name`, `price`, `description`, `quantity`, `brand_id`, `type`) VALUES ('".$_POST['product_name']."','".$_POST['price']."','".$_POST['description']."','".$_POST['quantity']."','".$_POST['brand_id']."','".$_POST['type']."')";
         
