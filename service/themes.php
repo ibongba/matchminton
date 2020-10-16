e66674711779eca65562c01bd51304817b456cd7
Anuparp Wijanpreecha <I.bong.ba@gmail.com>
2020-10-16 19:29:05 +0700

    Update themes.php

--- service/themes.php
+++ service/themes.php
@@ -6,7 +6,7 @@
     	
         $sql = "INSERT INTO `theme`(`content`, `title`, `category`, `fk_user_id`) VALUES ('".$_POST['summernote']."','".$_POST['header']."','".$_POST['category']."','".$_POST['user_id']."')";
         $rs = getpdo($conn,$sql);
-        if(isset($rs) && count($rs) > 0){
+        if(isset($rs)){
         	$res = array("code" => 200, "result" => $rs[0]);
         	echo json_encode($res);
             return ;
