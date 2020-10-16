77890f41f4ac4e107095cdaed0e5d60f79906316
Anuparp Wijanpreecha <I.bong.ba@gmail.com>
2020-10-16 19:28:11 +0700

    Update themes.php

--- service/themes.php
+++ service/themes.php
@@ -4,7 +4,7 @@
 
     if(isset($_POST['action']) && $_POST['action'] == 'create_theme'){
     	
-        $sql = "INSERT INTO `theme`(`content`, `title`, `category`, `fk_user_id`) VALUES ('$_POST['summernote']','$_POST['header']','$_POST['category']','$_POST['user_id']')";
+        $sql = "INSERT INTO `theme`(`content`, `title`, `category`, `fk_user_id`) VALUES ('".$_POST['summernote']."','".$_POST['header']."','".$_POST['category']."','".$_POST['user_id']."')";
         $rs = getpdo($conn,$sql);
         if(isset($rs) && count($rs) > 0){
         	$res = array("code" => 200, "result" => $rs[0]);
