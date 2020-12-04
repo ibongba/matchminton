daf160c2ecbd0c78d44c521e6fe3be4ecd122ac1
Anuparp Wijanpreecha <anuparp.w@sahapat.co.th>
2020-12-04 17:08:59 +0700

    update

--- service/config.php
+++ service/config.php
@@ -1,13 +1,12 @@
 <?php
-	date_default_timezone_set("Asia/Bangkok");
-    header('Content-type: text/html; charset=utf-8');
-    require_once 'PDO.php';
+date_default_timezone_set("Asia/Bangkok");
+header('Content-type: text/html; charset=utf-8');
+require_once 'PDO.php';
 
-    $db_host = 'localhost:8889';
-    $db_name = 'matchminton';
-    $db_user = 'root';
-    $db_pass = '1';
+$db_host = 'localhost';
+$db_name = 'matchminton';
+$db_user = 'root';
+$db_pass = '';
 
-    $conn = new PDO("mysql:host=$db_host; dbname=$db_name", $db_user, $db_pass);
-    $conn->exec("SET CHARACTER SET utf8");
-?>
+$conn = new PDO("mysql:host=$db_host; dbname=$db_name", $db_user, $db_pass);
+$conn->exec("SET CHARACTER SET utf8");
