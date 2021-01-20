239c1f1422fa2ff6fa2998d588cf7a1de9239634
lookyee <57711440+lookyee@users.noreply.github.com>
2021-01-20 21:45:10 +0700

    up

--- service/admin_order.php
+++ service/admin_order.php
@@ -2,7 +2,14 @@
     require_once 'config.php';
     
     if (isset($_POST['action']) && $_POST['action'] == 'show_admin_order'){
-        $sql_pro = "SELECT *, SUM(`product`.`quantity`) AS qauntity_product  FROM `orders` JOIN `users` ON `orders`.`user_id` = `users`.`user_id` JOIN `order_details` ON `orders`.`order_id` = `order_details`.`order_id` JOIN `product` ON `order_details`.`product_id` = `product`.`product_id` WHERE `stataus`= '1' AND `tracking` IS NULL  GROUP BY `orders`.`order_id` ORDER BY `orders`.`order_id` DESC";
+        $sql_pro = "SELECT *, SUM(`product`.`quantity`) AS qauntity_product  
+        FROM `orders` 
+        JOIN `users` ON `orders`.`user_id` = `users`.`user_id` 
+        JOIN `order_details` ON `orders`.`order_id` = `order_details`.`order_id` 
+        JOIN `product` ON `order_details`.`product_id` = `product`.`product_id` 
+        WHERE `stataus`= '1' AND `tracking` IS NULL  
+        GROUP BY `orders`.`order_id` 
+        ORDER BY `orders`.`order_id` DESC";
         $rs = getpdo($conn,$sql_pro);
 // echo $sql_pro;
         if(gettype($rs) == 'array'){
@@ -26,6 +33,33 @@
             $res = array("code" => 200, "result" => array("product" => $rs,"product_images" => $rs2));
         	echo json_encode($res);
             return ;
+        }
+    } else if (isset($_POST['action']) && $_POST['action'] == 'show_stock_admin_order'){
+        $sql_pro = "SELECT *, SUM(`product`.`quantity`) AS qauntity_product 
+        FROM `product` 
+        GROUP BY `product_id` 
+        ORDER BY `product_id`DESC ";
+        $rs = getpdo($conn,$sql_pro);
+// echo $sql_pro;
+        if(gettype($rs) == 'array'){
+
+            $sql = "SELECT * FROM `product_image`  WHERE `fk_product_id` in (SELECT `product_id` FROM `product` JOIN `racket_detail` ON `product`.`product_id` = `racket_detail`.`fk_product_id`)";
+            $rs2 = getpdo($conn,$sql);
+
+            $res = array("code" => 200, "result" => array("product" => $rs,"product_images" => $rs2));
+        	echo json_encode($res);
+            return ;
+        }
+    }else if (isset($_POST['action']) && $_POST['action'] == 'create_stock') {
+
+        $sql = "UPDATE `product` SET `quantity`= '".$_POST['qauntity']."' WHERE `product_id` = '".$_POST['product_id']."'";
+        // echo $sql;
+        $rs = getpdo($conn, $sql);
+    
+        if (isset($rs)) {
+            $res = array("code" => 200, "result" => $rs, "sql" => $sql);
+            echo json_encode($res);
+            return;
         }
     }
 
