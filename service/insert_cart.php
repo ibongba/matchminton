120cc40ff16fac0516199ec7abc5e5fba3e457f6
Anuparp Wijanpreecha <kews@Anuparps-Mac-mini.local>
2021-02-05 03:54:06 +0700

    update

--- service/insert_cart.php
+++ service/insert_cart.php
@@ -3,29 +3,29 @@
 ?>
 
 <?php
-  if(isset($_GET["user_id"])){
+  if(isset($_POST["user_id"])){
     $date = date_create();
     $date_now = date_format($date, 'Y-m-d H:i:s') . "\n";
-    $sql = "SELECT * FROM `cart` WHERE `user_id` = '".$_GET['user_id']."' and `product_id` = '".($_GET['product_id'])."'";
+    $sql = "SELECT * FROM `cart` WHERE `user_id` = '".$_POST['user_id']."' and `product_id` = '".($_POST['product_id'])."'";
     $rs = getpdo($conn,$sql);
     if(isset($rs) && count($rs) > 0){
       $amount = $rs[0]['amount'] + 1;
       $sql = "UPDATE `cart` SET `amount`= '".$amount."' WHERE `cart_id` = '".$rs[0]['cart_id']."'";
       $rs = getpdo($conn,$sql);
     }else{
-      $sql = "INSERT INTO `cart`(`user_id`, `product_id`, `amount`,`created_at`,`updated_at`,`remark`) VALUES ('".$_GET['user_id']."','".$_GET['product_id']."',";
+      $sql = "INSERT INTO `cart`(`user_id`, `product_id`, `amount`,`created_at`,`updated_at`,`remark`) VALUES ('".$_POST['user_id']."','".$_POST['product_id']."',";
 
-      if(isset($_GET['amount']) == ''){
+      if(isset($_POST['amount']) == ''){
         $sql .= " '1' ";
       }else{
-        $sql .= " '".$_GET['amount']."' ";
+        $sql .= " '".$_POST['amount']."' ";
       }
       $sql .= ", '".$date_now."', '".$date_now."', ";
 
-      if (isset($_GET['remark']) == '') {
+      if (isset($_POST['remark']) == '') {
         $sql .= " '' ";
       } else {
-        $sql .= " '".$_GET['remark']."' ";
+        $sql .= " '".$_POST['remark']."' ";
       }
       $sql .= " ) ";
       // echo $sql;
@@ -34,10 +34,10 @@
    
     if($rs){
       $res = array("code" => 200, "result" => $rs);
-    echo json_encode($res);
+      echo json_encode($res);
       return ;
 
-  }
+    }
     // if(!$rs)
     // {
     //   echo "<script>";
@@ -50,5 +50,8 @@
     //     echo "window.history.back()";
     //   echo "</script>";
     // }
+    $result = array("message" => "Error someting");
+    $res = array("code" => 401, "result" => $result, "sql" => $sql );
+    echo json_encode($res);
   }
 ?>
